fecea369839eaabfaa45e1a8f6c72b504df79f6d
Sunconure11 <eyespot7b@aol.com>
2021-02-19 16:47:45 -0500

    Tweaks

--- src/main/java/info/ata4/minecraft/dragon/server/block/BlockDragonBreedEgg.java
+++ src/main/java/info/ata4/minecraft/dragon/server/block/BlockDragonBreedEgg.java
@@ -16,8 +16,13 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
  * @author Nico Bergemann <barracuda415 at yahoo.de>
@@ -52,6 +57,15 @@ public class BlockDragonBreedEgg extends BlockDragonEgg {
 	public int getMetaFromState(IBlockState state) {
 		EnumDragonBreed type = state.getValue(BREED);
 		return EnumDragonBreed.META_MAPPING.get(type);
+	}
+
+	@Override
+	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		return false;
 	}
 
 	@Override
