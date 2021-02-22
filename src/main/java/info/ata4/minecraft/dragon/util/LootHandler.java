2d0ff747cbbde8027b863c0271ac9e773133ab9a
Sunconure11 <eyespot7b@aol.com>
2021-02-22 18:52:15 -0500

    Update LootHandler.java

--- src/main/java/info/ata4/minecraft/dragon/util/LootHandler.java
+++ src/main/java/info/ata4/minecraft/dragon/util/LootHandler.java
@@ -1,11 +1,17 @@
 package info.ata4.minecraft.dragon.util;
 
+import info.ata4.minecraft.dragon.DragonMounts;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.*;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class LootHandler {
     @SubscribeEvent
     public void lootLoad(LootTableLoadEvent evt) {
-
+        if (evt.getName().equals(LootTableList.CHESTS_NETHER_BRIDGE)) {
+            evt.getTable().addPool(new LootPool(new LootEntry[]{new LootEntryTable(new ResourceLocation(DragonMounts.ID, "chests/nether_materials"), 5, 0, new LootCondition[0], "bewitchment_nether_materials_entry")}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "bewitchment_nether_materials_pool"));
+        }
     }
 }
