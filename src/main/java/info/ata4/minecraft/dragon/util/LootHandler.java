42594de8b1ad03fd9fb0f01398e9ba0f926b5f3f
Sunconure11 <eyespot7b@aol.com>
2021-02-23 03:21:20 -0500

    Begin on loot tables.

--- src/main/java/info/ata4/minecraft/dragon/util/LootHandler.java
+++ src/main/java/info/ata4/minecraft/dragon/util/LootHandler.java
@@ -8,10 +8,10 @@ import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class LootHandler {
-    @SubscribeEvent
-    public void lootLoad(LootTableLoadEvent evt) {
-        if (evt.getName().equals(LootTableList.CHESTS_NETHER_BRIDGE)) {
-            evt.getTable().addPool(new LootPool(new LootEntry[]{new LootEntryTable(new ResourceLocation(DragonMounts.ID, "chests/nether_materials"), 5, 0, new LootCondition[0], "bewitchment_nether_materials_entry")}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "bewitchment_nether_materials_pool"));
-        }
-    }
+	@SubscribeEvent
+	public void lootLoad(LootTableLoadEvent evt) {
+		if (evt.getName().equals(LootTableList.CHESTS_NETHER_BRIDGE)) {
+			evt.getTable().addPool(new LootPool(new LootEntry[]{new LootEntryTable(new ResourceLocation(DragonMounts.AID, "chests/nether_egg"), 5, 0, new LootCondition[0], "nether_egg")}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "nether_egg"));
+		}
+	}
 }
